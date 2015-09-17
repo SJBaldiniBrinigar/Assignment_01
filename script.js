6e273739f618e12fe5192fb4d1fd4dcd80504c7c
SBaldiniBrinigar <baldinisj@aol.com>
2015-09-17 00:35:40 -0700

    Assignment 1 commit

---
+++ script.js
@@ -0,0 +1,239 @@
+// Step 1:
+
+// var someMonth;
+// function theCurrentMonth(); 
+// constCurrentMonth
+// var allSummerMonths;
+// function myLibrary();
+
+
+
+// Step 2 (literal expressions):
+
+//var isNumber = 2.5;
+//window.console.log(isNumber);
+
+//var isString = "This is a string.";
+//window.console.log(isString);
+
+//var x = false;
+//window.console.log(x);
+
+//var x = null;
+//window.console.log(x);
+
+
+
+//Step 3 (2 exmaples of complex/variable expressions):
+
+//var x = 5;
+//var y = 40;
+//window.console.log(x*y)
+    
+//var x = 10;
+//var y = 4;
+//window.console.log(x >= y);
+
+
+
+//Step 4 (9 variables declared):
+
+//var strNirstName;
+//var strLastName;
+//var strAddress;
+//var strCity;
+//var strState;
+//var strZipCode;
+//var numYourAge;
+//var strReferralSource;
+//var blnMayWeContactYou;
+
+
+
+//Step 5 (3 methods to assign values to variables):
+
+//var strFirstName;
+//var strLastName;
+//strFirstName = "Sandy";
+//strLastName = "BaldiniBrinigar";
+
+//var strFirstName = "Sandy";
+//var strLastName = "BaldiniBrinigar";
+
+//var strFirstName = "Sandy", strLastName = "BaldiniBrinigar";
+
+
+
+//Step 6 (adding variables number & string; boolean & string; boolean & number);
+
+//var x = 2.5;
+//var y = "Sandy";
+//window.console.log(x+y);
+
+//Result: 2.5Sandy
+
+//var x = false;
+//var y = "Sandy";
+//window.console.log(x+y);
+
+//Result: falseSandy
+
+//var x = true;
+//var y = 6;
+//window.console.log(x+y);
+
+//Result 7
+
+
+
+//Step 7  (Hoisting; write program returns undefined): 
+
+//var firstName = "Sandy";
+//window.console.log("The student's name is " + firstName + " " + lastName + ".");
+
+
+//var lastName = "BaldiniBrinigar";
+
+//Result "The student's name is Sandy undefined."
+
+
+
+//Step 8  (literal string corrected to valid using \ before ')
+
+//var someString = 'Who once said, "Only two things are infinite, the universe //and human stupidity, and I\'m not sure about the former."';
+//window.console.log(someString);
+
+
+
+//Step 9  (variable that produces a null in console and variable that produces undefined in console):
+
+//var x = 15;
+//x = null;
+//window.console.log(x);
+
+//Result null
+
+
+//var x;
+//window.console.log(x);
+
+//Result undefined
+
+
+
+//Step 10  (typeof for string, number, Boolean, object and undefined)
+
+//var name = "Sandy";
+//window.console.log(typeof name);
+
+//var x = 35.6;
+//window.console.log(typeof x);
+
+//var x = false;
+//window.console.log(typeof x);
+
+//var book = new Object();
+//window.console.log(typeof book);
+
+//var x;
+//window.console.log(typeof x);
+
+
+
+// Step 11  (alert box)
+
+//window.alert("Hello" + " " + "Sandy Baldini Brinigar" + ", welcome to the Javascript class!");
+
+
+
+// Step 12 (alert box with 1 variable)
+
+//var name = "Sandy BaldiniBrinigar";
+//window.alert("Hello" + " " + name + ", welcome to the Javascript class!");
+
+
+
+// Step 13 (alert box with 2 variables)
+
+//var name = "Sandy BaldiniBrinigar";
+//var course = "Javascript";
+//window.alert("Hello" + " " + name + ", welcome to the " + course + " class!");
+
+
+
+// Step 14  (with line break)
+
+//var name = "Sandy BaldiniBrinigar";
+//var course = "Javascript";
+//window.alert("Hello" + " " + name + ".\nWelcome to the " + //course + " class!");
+
+
+// Step 15  (name prompt)
+
+//var course = "Javascript";
+//var name = prompt("What is your name?");
+//window.alert("Hello" + " " + name + ".\nWelcome to the " + //course + " class!");              
+
+
+// Step 16  (course prompt)
+
+//var name = prompt("What is your name?");
+//var course = prompt("What class are you taking?");
+//window.alert("Hello" + " " + name + ".\nWelcome to the " + course + " class!"); 
+
+
+// Step 17  (addition)
+
+//var x = 10;
+//var y = 20;
+//window.console.log(x+y);
+
+
+// Step 18  (add and assign)
+
+//var x = 20;
+//window.console.log(x += 20);
+
+
+// Step 19  (multiply and assign);
+
+//var x = 20;
+//window.console.log(x *= 5);
+
+
+// Step 20  (remainder)
+
+//var x = (20 % 3)
+//window.console.log(x /=1);
+
+
+// Step 21  (comparison/logical to evaluate to true)
+
+//var x = 15;
+//var y = 30;
+//window.console.log(x <= y);
+
+
+// Step 22  (comparison/logical to evaluate to false)
+
+//var x = 15;
+//var y = 30;
+//window.console.log(x >= y);
+
+// Step 23  (new Object)
+
+//var widget = new Object();
+//window.console.log(typeof widget);
+
+
+// Step 24 (instanceof)
+
+//var widget = new Object();
+//window.console.log(widget instanceof Object);
+
+
+// Step 25 (instanceof to be false)
+
+//var widget = new Object();
+//window.console.log(widget instanceof String);
+
